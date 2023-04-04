912e56764650bdddb89f5c80ca1a56154bf4570f
LegendaryPaul <paulanjelozzz@gmail.com>
2023-04-04 15:31:36 +0800

    BLOG BATCH07

---
+++ includes/theme-support.php
@@ -0,0 +1,12 @@
+<?php
+
+function blog_support(){
+    register_nav_menus(
+        array(
+            'header_menu' => __('header menu', 'text_domain')
+        )
+    );
+    add_theme_support('menus');
+    add_theme_support('post-thumbnails');
+}
+add_action('after_setup_theme', 'blog_support');
